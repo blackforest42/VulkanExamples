450c5d78fb79062f571f310c51ab9b57c9ed8116
Jeff Ahn <ahn.jeff@gmail.com>
2025-09-22 21:34:28 -0700

    add png for blackhole cubemaps and textures. test stb works

--- examples/blackhole/blackhole.cpp
+++ examples/blackhole/blackhole.cpp
@@ -14,6 +14,7 @@
 #include <ktx.h>
 #include <ktxvulkan.h>
 #include "VulkanglTFModel.h"
+#include "stb_image.h"
 #include "vulkanexamplebase.h"
 
 class VulkanExample : public VulkanExampleBase {
@@ -80,33 +81,24 @@ class VulkanExample : public VulkanExampleBase {
     }
   }
 
+  void createTextureImage() {
+    int texWidth, texHeight, texChannels;
+    stbi_uc* pixels =
+        stbi_load((getAssetPath() + "textures/blackhole_color_map.png").c_str(),
+                  &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+    VkDeviceSize imageSize = texWidth * texHeight * 4;
+
+    if (!pixels) {
+      throw std::runtime_error("failed to load texture image!");
+    }
+  }
+
   // Loads a cubemap from a file, uploads it to the device and create all Vulkan
   // resources required to display it
   void loadCubemap(std::string filename, VkFormat format) {
     ktxResult result;
     ktxTexture* ktxTexture;
 
-#if defined(__ANDROID__)
-    // Textures are stored inside the apk on Android (compressed)
-    // So they need to be loaded via the asset manager
-    AAsset* asset = AAssetManager_open(androidApp->activity->assetManager,
-                                       filename.c_str(), AASSET_MODE_STREAMING);
-    if (!asset) {
-      vks::tools::exitFatal("Could not load texture from " + filename +
-                                "\n\nMake sure the assets submodule has been "
-                                "checked out and is up-to-date.",
-                            -1);
-    }
-    size_t size = AAsset_getLength(asset);
-    assert(size > 0);
-
-    ktx_uint8_t* textureData = new ktx_uint8_t[size];
-    AAsset_read(asset, textureData, size);
-    AAsset_close(asset);
-    result = ktxTexture_CreateFromMemory(
-        textureData, size, KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &ktxTexture);
-    delete[] textureData;
-#else
     if (!vks::tools::fileExists(filename)) {
       vks::tools::exitFatal("Could not load texture from " + filename +
                                 "\n\nMake sure the assets submodule has been "
@@ -115,7 +107,6 @@ class VulkanExample : public VulkanExampleBase {
     }
     result = ktxTexture_CreateFromNamedFile(
         filename.c_str(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &ktxTexture);
-#endif
     assert(result == KTX_SUCCESS);
 
     // Get properties required for using and upload texture data from the ktx
@@ -310,6 +301,8 @@ class VulkanExample : public VulkanExampleBase {
     // Cubemap texture
     loadCubemap(getAssetPath() + "textures/cubemap_yokohama_rgba.ktx",
                 VK_FORMAT_R8G8B8A8_UNORM);
+
+    createTextureImage();
   }
 
   void setupDescriptors() {
